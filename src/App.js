983ffb182381d437be9bd307367010204079e280
kirshr <astudillokiran@gmail.com>
2023-01-04 08:18:38 -0700

    Created authentication using firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Home from "./Routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "../src/Routes/navigation/navigation.component";
-import SignIn from "./components/sign-in/sign-in.component";
+import Authentication from "./components/authentication/authentication.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="signIn" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
